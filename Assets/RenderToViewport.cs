67d49697914033a712f4942471518c55d77e8a15
matt.gooding@gmail.com <matt.gooding@gmail.com>
2018-03-24 10:25:58 -0400

    initial commit

---
+++ Assets/RenderToViewport.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class RenderToViewport : MonoBehaviour
+{
+    private Texture2D _texture;
+    private byte[] _textureBytes;
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (_texture == null)
+        {
+            _texture = new Texture2D(src.width, src.height, TextureFormat.ARGB32, false, false);
+            _textureBytes = new byte[src.width * src.height * 4];
+        }
+
+        _texture.LoadRawTextureData(_textureBytes);
+        Graphics.Blit(_texture, dest);
+    }
+}
