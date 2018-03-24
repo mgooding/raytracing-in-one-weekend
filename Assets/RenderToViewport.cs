b9edf767c48cbb0495626943c639e7671daac9b2
matt.gooding@gmail.com <matt.gooding@gmail.com>
2018-03-24 15:22:09 -0400

    Chapter 4

--- Assets/RenderToViewport.cs
+++ Assets/RenderToViewport.cs
@@ -24,8 +24,21 @@ public class RenderToViewport : MonoBehaviour
         Debug.Break(); // so slow, just break after a frame to maintain interactivity in editor
     }
 
+    private bool HitSphere(Vector3 center, float radius, Ray ray)
+    {
+        Vector3 oc = ray.origin - center;
+        float a = Vector3.Dot(ray.direction, ray.direction);
+        float b = 2.0f * Vector3.Dot(oc, ray.direction);
+        float c = Vector3.Dot(oc, oc) - radius * radius;
+        float discriminant = b * b - 4 * a * c;
+        return discriminant > 0.0f;
+    }
+
     private Vector3 ColorFromRay(Ray ray)
     {
+        if (HitSphere(new Vector3(0, 0, -1.0f), 0.5f, ray))
+            return new Vector3(1.0f, 0.0f, 0.0f);
+
         Vector3 normalizedDir = ray.direction.normalized;
         float t = 0.5f * (normalizedDir.y + 1.0f);
         return (1.0f - t) * Vector3.one + t * new Vector3(0.5f, 0.7f, 1.0f);
