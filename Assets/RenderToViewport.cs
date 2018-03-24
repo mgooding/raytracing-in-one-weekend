56019fb1b39c4b9fcdef85703bbd9c85e1aa7b4c
matt.gooding@gmail.com <matt.gooding@gmail.com>
2018-03-24 15:33:01 -0400

    Chapter 5

--- Assets/RenderToViewport.cs
+++ Assets/RenderToViewport.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class RenderToViewport : MonoBehaviour
 {
@@ -15,7 +16,13 @@ public class RenderToViewport : MonoBehaviour
             _textureBytes = new byte[src.width * src.height * 4];
         }
 
-        RenderToBytes(_texture.width, _texture.height);
+        var spheres = new SphereRecord[]
+        {
+            new SphereRecord {Center = new Vector3(0.0f, 0.0f, -1.0f), Radius = 0.5f},
+            new SphereRecord {Center = new Vector3(0.0f, -100.5f, -1.0f), Radius = 100.0f}
+        };
+
+        RenderToBytes(_texture.width, _texture.height, spheres);
         _texture.LoadRawTextureData(_textureBytes);
         _texture.Apply();
 
@@ -24,27 +31,81 @@ public class RenderToViewport : MonoBehaviour
         Debug.Break(); // so slow, just break after a frame to maintain interactivity in editor
     }
 
-    private bool HitSphere(Vector3 center, float radius, Ray ray)
+    private class HitRecord
     {
-        Vector3 oc = ray.origin - center;
-        float a = Vector3.Dot(ray.direction, ray.direction);
-        float b = 2.0f * Vector3.Dot(oc, ray.direction);
-        float c = Vector3.Dot(oc, oc) - radius * radius;
-        float discriminant = b * b - 4 * a * c;
-        return discriminant > 0.0f;
+        public float T;
+        public Vector3 Point;
+        public Vector3 Normal;
     }
 
-    private Vector3 ColorFromRay(Ray ray)
+    private struct SphereRecord
     {
-        if (HitSphere(new Vector3(0, 0, -1.0f), 0.5f, ray))
-            return new Vector3(1.0f, 0.0f, 0.0f);
+        public Vector3 Center;
+        public float Radius;
+    }
+
+    private void HitSphere(SphereRecord sphere, Ray ray, float tMin, float tMax, out HitRecord hitRecord)
+    {
+        Vector3 oc = ray.origin - sphere.Center;
+        float a = Vector3.Dot(ray.direction, ray.direction);
+        float b = Vector3.Dot(oc, ray.direction);
+        float c = Vector3.Dot(oc, oc) - sphere.Radius * sphere.Radius;
+        float discriminant = b * b - a * c;
+        if (discriminant < 0.0f)
+        {
+            hitRecord = null;
+            return;
+        }
+
+        float temp = (-b - Mathf.Sqrt(discriminant)) / a;
+        if (temp < tMax && temp > tMin)
+        {
+            Vector3 point = ray.GetPoint(temp);
+            hitRecord = new HitRecord() { T = temp, Point = point, Normal = (point - sphere.Center) / sphere.Radius };
+            return;
+        }
+
+        temp = (-b + Mathf.Sqrt(discriminant)) / a;
+        if (temp < tMax && temp > tMin)
+        {
+            Vector3 point = ray.GetPoint(temp);
+            hitRecord = new HitRecord() { T = temp, Point = point, Normal = (point - sphere.Center) / sphere.Radius };
+            return;
+        }
+
+        hitRecord = null;
+    }
+
+    private void HitSphereArray(IEnumerable<SphereRecord> spheres, Ray ray, float tMin, float tMax, out HitRecord hitRecord)
+    {
+        float closestHit = tMax;
+        hitRecord = null;
+
+        foreach (SphereRecord sphere in spheres)
+        {
+            HitRecord tmpHit;
+            HitSphere(sphere, ray, tMin, closestHit, out tmpHit);
+            if (tmpHit != null)
+            {
+                hitRecord = tmpHit;
+                closestHit = tmpHit.T;
+            }
+        }
+    }
+
+    private Vector3 ColorFromRay(Ray ray, IEnumerable<SphereRecord> spheres)
+    {
+        HitRecord hitRecord;
+        HitSphereArray(spheres, ray, 0.0f, float.MaxValue, out hitRecord);
+        if (hitRecord != null)
+            return 0.5f * (hitRecord.Normal + Vector3.one);
 
         Vector3 normalizedDir = ray.direction.normalized;
         float t = 0.5f * (normalizedDir.y + 1.0f);
         return (1.0f - t) * Vector3.one + t * new Vector3(0.5f, 0.7f, 1.0f);
     }
 
-    private void RenderToBytes(int width, int height)
+    private void RenderToBytes(int width, int height, SphereRecord[] spheres)
     {
         int bytesOffset = 0;
 
@@ -60,7 +121,7 @@ public class RenderToViewport : MonoBehaviour
                 float u = (float) i / (float) width;
                 float v = (float) j / (float) height;
 
-                Vector3 rgb = ColorFromRay(new Ray(origin, lowerLeft + u * horizontal + v * vertical));
+                Vector3 rgb = ColorFromRay(new Ray(origin, lowerLeft + u * horizontal + v * vertical), spheres);
 
                 _textureBytes[bytesOffset++] = (byte)(255.99f * rgb.x);
                 _textureBytes[bytesOffset++] = (byte)(255.99f * rgb.y);
