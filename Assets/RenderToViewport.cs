7892581fe6e82bb51ca8c2379dbc6c3a59640fe3
matt.gooding@gmail.com <matt.gooding@gmail.com>
2018-03-24 15:14:37 -0400

    Chapter 3

--- Assets/RenderToViewport.cs
+++ Assets/RenderToViewport.cs
@@ -20,23 +20,38 @@ public class RenderToViewport : MonoBehaviour
         _texture.Apply();
 
         Graphics.Blit(_texture, dest);
+
+        Debug.Break(); // so slow, just break after a frame to maintain interactivity in editor
+    }
+
+    private Vector3 ColorFromRay(Ray ray)
+    {
+        Vector3 normalizedDir = ray.direction.normalized;
+        float t = 0.5f * (normalizedDir.y + 1.0f);
+        return (1.0f - t) * Vector3.one + t * new Vector3(0.5f, 0.7f, 1.0f);
     }
 
     private void RenderToBytes(int width, int height)
     {
         int bytesOffset = 0;
 
+        Vector3 lowerLeft = new Vector3(-2.0f, -1.0f, -1.0f);
+        Vector3 horizontal = new Vector3(4.0f, 0.0f, 0.0f);
+        Vector3 vertical = new Vector3(0.0f, 2.0f, 0.0f);
+        Vector3 origin = Vector3.zero;
+
         for (int j = 0; j < height; ++j)
         {
             for (int i = 0; i < width; ++i)
             {
-                float r = (float)i / (float)width;
-                float g = (float) j / (float) height;
-                const float b = 0.2f;
+                float u = (float) i / (float) width;
+                float v = (float) j / (float) height;
 
-                _textureBytes[bytesOffset++] = (byte)(255.99f * r);
-                _textureBytes[bytesOffset++] = (byte)(255.99f * g);
-                _textureBytes[bytesOffset++] = (byte)(255.99f * b);
+                Vector3 rgb = ColorFromRay(new Ray(origin, lowerLeft + u * horizontal + v * vertical));
+
+                _textureBytes[bytesOffset++] = (byte)(255.99f * rgb.x);
+                _textureBytes[bytesOffset++] = (byte)(255.99f * rgb.y);
+                _textureBytes[bytesOffset++] = (byte)(255.99f * rgb.z);
                 _textureBytes[bytesOffset++] = 255;
             }
         }
